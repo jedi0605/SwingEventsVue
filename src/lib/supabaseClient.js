88809f765b18cb8f78bbff827fb8200e9e171bb8
jedi <jedi2454@gmail.com>
2023-03-31 10:17:44 +0800

    init vue, connect to supabase

---
+++ src/lib/supabaseClient.js
@@ -0,0 +1,3 @@
+  import { createClient } from '@supabase/supabase-js'
+
+  export const supabase = createClient('https://meleqkzyxdjdfvowkbxg.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1lbGVxa3p5eGRqZGZ2b3drYnhnIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODAxODc4OTcsImV4cCI6MTk5NTc2Mzg5N30.xjtCmQFEzJeI3HQJvZsxxjfKQHFYJIUPY1cgkVK-yFU')
---
+++ src/views/AboutView.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+import { supabase } from '../lib/supabaseClient'
+
+const countries = ref([])
+
+async function getCountries() {
+  const { data } = await supabase.from('countries').select()
+  countries.value = data
+}
+
+onMounted(() => {
+  getCountries()
+})
+</script>
+
+<template>
+  <ul>
+    <li v-for="country in countries" :key="country.id">{{ country.name }}</li>
+  </ul>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
